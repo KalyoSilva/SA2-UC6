0204b8cd4f057726c18960b9de388936892300b9
KalyoSilva <85971839+KalyoSilva@users.noreply.github.com>
2021-06-29 19:48:46 -0300

    Delete ex4.py

--- ex4.py
+++
@@ -1,9 +0,0 @@
-while(True):
-    try:
-        size = int(input("Informe o tamanho da linha: "))
-        break
-    except:
-        print("Comando inválido, tente novamente.")
-
-for x in range(size):
-    print("_", end ="")
